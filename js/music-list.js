fdf5abd1cba1bc735a810f2ceccb9315a2086c4a
Aditya Kumar Dwivedi <akdwivedi7355@gmail.com>
2024-06-15 11:39:49 +0530

    main

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+
+    {
+        name: "Tu Hai Kahan",
+        artist: "Aur",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Ve kamleya",
+        artist: "Audio Library",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Pehle Bhi Main",
+        artist: "Vishal Mishra",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Jeena jeena kaise jeena",
+        artist: "Atif aslam",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Tera Hone Laga Hu",
+        artist: "Atif Aslam",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Dekhte Dekhte",
+        artist: "Atif aslam",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Heeriye Heeriye",
+        artist: "arjit singh",
+        img: "music-7",
+        src: "music-7"
+    }
+    ];
